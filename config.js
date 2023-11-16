d84c7f163aa0c2aaf6957806bdcb44f47c75cdc8
lankeshwhatsapp4444 <151029357+lankeshwhatsapp4444@users.noreply.github.com>
2023-11-17 03:50:46 +0530

    Update config.js

--- config.js
+++ config.js
@@ -14,18 +14,18 @@ function convertToBool(text, fault = 'true') {
 
 // PUT YOUR SESSION ID HERE  ##Required 
 
-global.SESSION_ID = '' // PUT your session ID BY scan QR 
+global.SESSION_ID = 'QueenNilu;;;1yIjVC6R#OIMhbS8kNlzv0xchuD6L_KrDkQNL4F6Ub6fYT3-vxKg' // PUT your session ID BY scan QR 
 
 // databse URL  ##Required 
-global.POSTGRESQL_URL = ''
+global.POSTGRESQL_URL = 'postgres://lankesh_user:2RmDq8MmTRnc6dj7gmIVyW1gA0w0lfsm@dpg-clb8ncug1b2c73d2atcg-a.frankfurt-postgres.render.com/lankesh'
 
  
 
 // ⚙️ OWNER SETTINGS  ⚙️
 
-global.OWNER_NUMBER = '94767438882'
+global.OWNER_NUMBER = '94772609337'
 
-global.OWNER_NAME = 'Janiya_X'
+global.OWNER_NAME = 'Lankesh Sandaruwan'
 
 global.TIMEZONE = 'Asia/Colombo' 
 
@@ -47,17 +47,17 @@ global.FOOTER = 'Queen Nilu 2023'
 
 global.BOT_OFFLINE = false    //  Always Onlne -  | false offline -True
 
-global.STICKER_NAME = '@Janiya 🤹‍♂️' //sticker
+global.STICKER_NAME = '@Lankesh🙂' //sticker
 
-global.LANG = 'EN' // Language (EN-  TO english  ,  SI- TO sinhala )
+global.LANG = 'SI' // Language (EN-  TO english  ,  SI- TO sinhala )
 
 global.PREFIX = '.' // Bot Prefix
 
-global.CAPTION = 'ǫᴜᴇᴇɴ ɴɪʟᴜ  © 2023' // Caption
+global.CAPTION = 'Poewr by Lankesh © 2023' // Caption
 
-global.ALIVELOGO = 'https://telegra.ph/file/1e63f0ee90304a12767c7.jpg' // Set Alive Logo link 
+global.ALIVELOGO = 'https://www.facebook.com/share/M4HhvDWx6sQnTGpk/?mibextid=HqNPNq' // Set Alive Logo link 
 
-global.ALIVE_MESSAGE = '' // Set Alive Message
+global.ALIVE_MESSAGE = 'https://wa.me/message/MO5WD73YMT4OK1' // Set Alive Message
 
 global.MAX_SIZE = '500' // Bot Uloading Max size 
 
@@ -67,17 +67,17 @@ global.ANTI_BAD_ACTION = false
 
 global.ANTI_LINK = true //Anti Link (on = True , Off = False )
 
-global.ANTI_BOTS = true
+global.ANTI_BOTS = false
 
 global.ONLY_GROUP = 'false' // Only group mode
 
-global.GROUP_LINK = 'https://chat.whatsapp.com/IRdyJY5P4sF5BhtP5PIdHH' // ur group link
+global.GROUP_LINK = 'https://wa.me/message/MO5WD73YMT4OK1' // ur group link
 
 
 /* 18 + Download Settings */
 
-global.SEX_DL = 'false' // 18 + video dl
-
+global.SEX_DL = 'true' // 18 + video dl
+      .xnxx.com= 'true'
 /* AUTO REPLY SETTINGS */
 
 global.AUTO_STICKER = true
@@ -89,6 +89,6 @@ global.VOICE_REPLY = true
 
 /* V card */
 
-global.ytname = "YT: janithsadanuwan" //ur yt chanel name
-global.socialm = "GitHub: janithsadanuwan" //ur github or insta name
+global.ytname = "YT: Lankesh Sandaruwan " //ur yt chanel name
+global.socialm = "GitHub: LankeshSadaruwan" //ur github or insta name
 global.location = "Srilanka, colombo" //ur location
